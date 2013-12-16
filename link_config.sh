7c2c252764f14e8ffef16692ebe4757da36af9c2
Rafał Mirończyk <voyageur.pl@gmail.com>
2013-12-16 22:52:37 +0100

    don't link .git, lol

--- link_config.sh
+++ link_config.sh
@@ -3,7 +3,6 @@ ln -si `pwd`/.bash_aliases ~
 ln -si `pwd`/.bash_prompt ~
 ln -si `pwd`/.bashrc ~
 ln -si `pwd`/.config/terminator ~/.config/
-ln -si `pwd`/.git ~
 ln -si `pwd`/.gitconfig ~
 ln -si `pwd`/.gitfiles ~
 ln -si `pwd`/.q3a ~
