b627e7a3a54f695f037573f99b93ec3ef51aba40
Rafał Mirończyk <voyageur.pl@gmail.com>
2013-12-16 22:26:25 +0100

    add config installation script

---
+++ link_config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ln -si `pwd`/.bash_aliases ~
+ln -si `pwd`/.bash_prompt ~
+ln -si `pwd`/.bashrc ~
+ln -si `pwd`/.config/terminator ~/.config/
+ln -si `pwd`/.git ~
+ln -si `pwd`/.gitconfig ~
+ln -si `pwd`/.gitfiles ~
+ln -si `pwd`/.q3a ~
+ln -si `pwd`/.ssh ~/.ssh
+ln -si `pwd`/.vimrc ~
+sudo ln -si `pwd`/etc/hosts /etc/
