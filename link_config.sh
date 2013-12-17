380cfbf9dfc24465f83c6093bdf4d7034de80a0f
Rafał Mirończyk <voyageur.pl@gmail.com>
2013-12-17 10:35:00 +0100

    fix dirs symlinking

--- link_config.sh
+++ link_config.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
 ln -si `pwd`/.bash_aliases ~
 ln -si `pwd`/.bash_prompt ~
 ln -si `pwd`/.bashrc ~
-ln -si `pwd`/.config/terminator ~/.config/
-ln -si `pwd`/.gitconfig ~
-ln -si `pwd`/.gitfiles ~
-ln -si `pwd`/.q3a ~
-ln -si `pwd`/.ssh ~/.ssh
+ln -si `pwd`/.gitfiles/global-ignore ~/.gitfiles/
+ln -si `pwd`/.ssh/config ~/.ssh
 ln -si `pwd`/.vimrc ~
+ln -si `pwd`/.q3a/q3ut4/config.cfg ~/.q3a/q3ut4/
+ln -si `pwd`/.config/terminator/config ~/.config/terminator
+ln -si `pwd`/.gitconfig ~
+
 sudo ln -si `pwd`/etc/hosts /etc/
