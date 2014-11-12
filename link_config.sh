ec7f8f2c4dbd1ba1c2503dcb83843f4f00048f71
Rafał Mirończyk <voyageur.pl@gmail.com>
2014-11-12 10:30:32 +0100

    fix script for linking config

--- link_config.sh
+++ link_config.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-ln -si `pwd`/.bash_aliases ~
-ln -si `pwd`/.bash_prompt ~
-ln -si `pwd`/.bashrc ~
+ln -si `pwd`/.bash_aliases ~/.bash_aliases
+ln -si `pwd`/.bash_prompt ~/.bash_prompt
+ln -si `pwd`/.bashrc ~/.bashrc
 ln -si `pwd`/.bpython/* ~/.bpython/
-ln -si `pwd`/.gitfiles/global-ignore ~/.gitfiles/
-ln -si `pwd`/.ssh/config ~/.ssh
-ln -si `pwd`/.vimrc ~
-ln -si `pwd`/.q3a/q3ut4/config.cfg ~/.q3a/q3ut4/
-ln -si `pwd`/.config/terminator/config ~/.config/terminator
-ln -si `pwd`/.gitconfig ~
-ln -si `pwd`/.bash_completion ~
-ln -si `pwd`/.fab_bash_completion ~
-ln -si `pwd`/.git_bash_completion ~
+ln -si `pwd`/.gitfiles/global-ignore ~/.gitfiles/global-ignore
+ln -si `pwd`/.ssh/config ~/.ssh/config
+ln -si `pwd`/.vimrc ~/.vimrc
+ln -si `pwd`/.q3a/q3ut4/config.cfg ~/.q3a/q3ut4/config.cfg
+ln -si `pwd`/.config/terminator/config ~/.config/terminator/config
+ln -si `pwd`/.gitconfig ~/.gitconfig
+ln -si `pwd`/.bash_completion ~/.bash_completion
+ln -si `pwd`/.fab_bash_completion ~/.fab_bash_completion
+ln -si `pwd`/.git_bash_completion ~/.git_bash_completion
 
-sudo ln -si `pwd`/etc/hosts /etc/
+sudo ln -si `pwd`/etc/hosts /etc/hosts
